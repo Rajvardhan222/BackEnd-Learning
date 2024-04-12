de37cb3ccdd67c6881717c5bb97ab882a50bd06a
Rajvardhan <rajvardhanranawat80@gmail.com>
2024-04-12 10:31:29 +0530

    utils

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+
+let connectDB = async () => {
+        try {
+            
+           const connectionInstence =  await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+
+           console.log(connectionInstence.connection.host);
+        } catch (error) {
+            console.log('mongoDB Connection Error',error);
+            process.exit(1)
+        }
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+import app from './App.js'
+
+dotenv.config({
+    path:"./env"
+})
+let PORT = process.env.PORT || 8000
+connectDB()
+.then(()=>{
+    app.listen(PORT => {
+        console.log('Server is running on PORT',PORT );
+    })
+})
+.catch((err)=>{
+    console.log('Database connsction error',err);
+})
